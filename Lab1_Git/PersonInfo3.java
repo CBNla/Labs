e6d7f31aa5c5531d5d22cefd9fa2be5a7bb383aa
CBNla <36320250+CBNla@users.noreply.github.com>
2019-09-16 19:27:46 -0400

    Update PersonInfo3.java

--- Lab1_Git/PersonInfo3.java
+++ Lab1_Git/PersonInfo3.java
@@ -1,9 +1,9 @@
-public class PersonInfo3 {
+hpublic class PersonInfo3 {
 
-    public static String firstName = "Liz";
-    public static String lastName = "Grégoire";
-    public static int studentNo = 173819;
-    public static String description = "Likes long walks along the beach";
+    public static String firstName = "Cheikh";
+    public static String lastName = "Ndoye";
+    public static int studentNo = 300045791;
+    public static String description = "Just as it sounds";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
